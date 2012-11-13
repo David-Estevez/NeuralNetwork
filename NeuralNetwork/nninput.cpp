59e9a2e8bdfa0d3e34be4181efa306b0aa4db3e0
David-Estevez <david.estevez.fdez@gmail.com>
2012-11-13 19:51:43 +0100

    Destructor fixed. Code now is executed correctly.

--- NeuralNetwork/nninput.cpp
+++ NeuralNetwork/nninput.cpp
@@ -5,8 +5,8 @@ NNInput::~NNInput()
     //-- If allocated, free variables:
 
     for (int i = 0; i < (int) weights.size(); i++)
-	if ( weights.at( weights.size() - i ) != 0 )
-	    delete weights.at( weights.size() - i);
+	if ( weights.at( i ) != 0 )
+	    delete weights.at(i);
 
     if (input != 0)
 	delete input;
